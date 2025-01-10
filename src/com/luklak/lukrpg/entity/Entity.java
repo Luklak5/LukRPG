76d45852ba2d5e32eff038241c7e0f02ed623895
Lukas Laubert <l.laubert@seznam.cz>
2025-01-10 12:09:16 +0100

    Initial commit

---
+++ src/com/luklak/lukrpg/entity/Entity.java
@@ -0,0 +1,12 @@
+package com.luklak.lukrpg.entity;
+
+public class Entity
+{
+    private String name;
+    private int level;
+    private int str;
+    private int vit;
+    private int _int;
+    private int dex;
+    private int luk;
+}
